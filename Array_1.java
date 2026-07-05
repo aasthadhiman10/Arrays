ee2d3fad788d1081e2ae1026f99406bd73e70f96
Aastha Dhiman <aasthadhimann@gmail.com>
2026-07-05 18:19:40 +0530

    completed array basics practice code

---
+++ Array_1.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Array_1 {
+    public static void main(String[]args) {
+
+        int marks[] = new int [3];
+        marks[0] = 95;//physics
+        marks[1]= 96;//maths
+        marks[2]= 98;//Chemistry
+
+        // System.out.println(marks [0]);
+        // System.out.println(marks [1]);
+        // System.out.println(marks [2]);
+        
+        for(int i=0; i<=3; i++){
+        System.out.println(marks [i]);
+        }           
+    }   
+
+}
